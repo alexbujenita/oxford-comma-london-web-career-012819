4bcf747abe3c18d944903750459f3ef91d317353
Alex Bujenita <dekc89@gmail.com>
2018-12-09 12:41:57 +0000

    Struggle to understand this one

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
-def oxford_comma(array)
-
+def oxford_comma inp
+  if inp.length == 2
+    return "#{inp[0]} and #{inp[1]}"
+  elsif inp.length > 2
+    inp[-1].insert(0, "and ")
+  end
+  inp.join(", ")
 end
